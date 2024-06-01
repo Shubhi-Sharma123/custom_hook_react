8927a7775a58c97ae915445d501420a68b0ea9ee
Shubhi-Sharma123 <shubizz95@gmail.com>
2024-06-01 22:12:56 +0530

    first commit

---
+++ src/CustomHook.js
@@ -0,0 +1,24 @@
+import {useState,useEffect} from 'react'
+
+function CustomHook() {
+let [windim,setwindim]=useState({
+    width:window.innerWidth,
+    height:window.innerHeight
+})
+useEffect(()=>{
+    const hresize=()=>{
+        setwindim({
+            width:window.innerWidth,
+            height:window.innerHeight
+        })
+     
+        }
+        window.addEventListener('resize',hresize)
+        return ()=>{
+            window.removeEventListener('resize',hresize)
+    }
+},[])
+return windim
+}
+
+export default CustomHook
